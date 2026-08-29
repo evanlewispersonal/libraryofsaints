d395698988d7d5a2191f64650dab5df4d94d3a94
Evan Charles Lewis <evancharleslewis@icloud.com>
2026-08-29 20:12:39 +1000

    Cors fix

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const Saints = require('./data/saints')
 const PORT = process.env.PORT || 3004;
 
 var corsOptions = {
-    origin: ['https://libraryofsaints.com', 'https://www.library-of-saints-1c2e99c9c954.herokuapp.com/'],
+    origin: ['https://libraryofsaints.com', 'https://www.library-of-saints-1c2e99c9c954.herokuapp.com', 'http://www.library-of-saints-1c2e99c9c954.herokuapp.com'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
